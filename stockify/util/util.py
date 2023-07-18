536537471c67920639904237c60ed8c1b3f10085
pratikshinde115 <pratikshinde115@gmail.com>
2023-07-18 23:14:35 +0530

    config done

---
+++ stockify/util/util.py
@@ -0,0 +1,15 @@
+import yaml
+from stockify.exception import StockifyExpection
+import os,sys
+
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise StockifyExpection(e,sys) from e
